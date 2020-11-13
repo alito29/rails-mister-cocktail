98b4af0cd504e3488296d16c72db12a2a7ca2454
Ali Hamriti <hamritiali@gmail.com>
2020-11-13 16:25:53 +0100

    added picture migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_113848) do
+ActiveRecord::Schema.define(version: 2020_11_13_152432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_113848) do
     t.string "picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture_url"
   end
 
   create_table "doses", force: :cascade do |t|
